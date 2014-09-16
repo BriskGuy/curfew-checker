0ac26d4eb4917fc532543c3f09b22a6147b6e28b
Katie Hoffman <ktahoffman@gmail.com>
2014-09-16 11:45:30 -0400

    update spec syntax

--- spec/curfew_checker_spec.rb
+++ spec/curfew_checker_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe "simple_curfew_checker" do 
   it "should tell you if you're in trouble if it's 11" do 
-    STDOUT.should_receive(:puts).with("You're in trouble! Better get home quick!")
+    expect(STDOUT).to receive(:puts).with("You're in trouble! Better get home quick!")
     require_relative '../lib/simple_curfew_checker.rb'
     simple_curfew_checker(11)
   end
 
   it "should tell you if you're in trouble if it's later than 11" do 
-    STDOUT.should_receive(:puts).with("You're in trouble! Better get home quick!")
+    expect(STDOUT).to receive(:puts).with("You're in trouble! Better get home quick!")
     require_relative '../lib/simple_curfew_checker.rb'
     simple_curfew_checker(12)
   end
 
   it "should not tell you you're in trouble when time is before 11" do 
-    STDOUT.should_not_receive(:puts).with("You're in trouble! Better get home quick!")
+    expect(STDOUT).to_not recieve(:puts).with("You're in trouble! Better get home quick!")
     require_relative '../lib/simple_curfew_checker.rb'
     simple_curfew_checker(10)
   end
@@ -22,19 +22,19 @@ end
 
 describe "curfew_checker" do 
   it "should say you're in trouble if it's 11" do 
-    STDOUT.should_receive(:puts).with("You're in trouble! Better get home quick!")
+    expect(STDOUT).to receive(:puts).with("You're in trouble! Better get home quick!")
     require_relative '../lib/curfew_checker.rb'
     curfew_checker(11)
   end
 
   it "should say you're in trouble if it's later than 11" do 
-    STDOUT.should_receive(:puts).with("You're in trouble! Better get home quick!")
+    expect(STDOUT).to receive(:puts).with("You're in trouble! Better get home quick!")
     require_relative '../lib/curfew_checker.rb'
     curfew_checker(12)
   end
 
   it "should say you can keep having fun" do 
-    STDOUT.should_receive(:puts).with("It's all good. Keep having fun!")
+    expect(STDOUT).to receive(:puts).with("It's all good. Keep having fun!")
     require_relative '../lib/curfew_checker.rb'
     curfew_checker(9)
   end
@@ -42,19 +42,19 @@ end
 
 describe "complex_curfew_checker" do 
   it "should say you're in trouble if it's 11" do 
-    STDOUT.should_receive(:puts).with("Time to apparate!")
+    expect(STDOUT).to receive(:puts).with("Time to apparate!")
     require_relative '../lib/complex_curfew_checker.rb'
     complex_curfew_checker(11)
   end
 
   it "should say you're in trouble if it's later than 11" do 
-    STDOUT.should_receive(:puts).with("You're in trouble! Better get home quick!")
+    expect(STDOUT).to receive(:puts).with("You're in trouble! Better get home quick!")
     require_relative '../lib/complex_curfew_checker.rb'
     complex_curfew_checker(12)
   end
 
   it "should say you can keep having fun" do 
-    STDOUT.should_receive(:puts).with("It's all good. Keep having fun!")
+    expect(STDOUT).to receive(:puts).with("It's all good. Keep having fun!")
     require_relative '../lib/complex_curfew_checker.rb'
     complex_curfew_checker(9)
   end
@@ -62,19 +62,19 @@ end
 
 describe "deluxe_curfew_checker" do 
   it "should say you're in trouble if it's 11" do 
-    STDOUT.should_receive(:puts).with("Time to apparate!")
+    expect(STDOUT).to receive(:puts).with("Time to apparate!")
     require_relative '../lib/deluxe_curfew_checker.rb'
     deluxe_curfew_checker(11)
   end
 
   it "should say you're in trouble if it's later than 11" do 
-    STDOUT.should_receive(:puts).with("You're in trouble! Better get home quick!")
+    expect(STDOUT).to receive(:puts).with("You're in trouble! Better get home quick!")
     require_relative '../lib/deluxe_curfew_checker.rb'
     deluxe_curfew_checker(12)
   end
 
   it "should say you can keep having fun with the time you have left" do 
-    STDOUT.should_receive(:puts).with("You have 2 hour(s) left to keep having fun!")
+    expect(STDOUT).to receive(:puts).with("You have 2 hour(s) left to keep having fun!")
     require_relative '../lib/deluxe_curfew_checker.rb'
     deluxe_curfew_checker(9)
   end
@@ -82,19 +82,19 @@ end
 
 describe "platinum_curfew_checker" do 
   it "should say you're in trouble if it's 11 and curfew is at 11" do 
-    STDOUT.should_receive(:puts).with("Time to apparate!")
+    expect(STDOUT).to receive(:puts).with("Time to apparate!")
     require_relative '../lib/platinum_curfew_checker.rb'
     platinum_curfew_checker(11, 11)
   end
 
   it "should say you're in trouble if it's later than 11, which is your curfew" do 
-    STDOUT.should_receive(:puts).with("You're in trouble! Better get back to Hogwarts quick!")
+    expect(STDOUT).to receive(:puts).with("You're in trouble! Better get back to Hogwarts quick!")
     require_relative '../lib/deluxe_curfew_checker.rb'
     platinum_curfew_checker(12, 11)
   end
 
   it "should say you can keep having if curfew is 11 and time is 9 and say there are 2 hrs left" do 
-    STDOUT.should_receive(:puts).with("You have 2 hour(s) left to keep having fun!")
+    expect(STDOUT).to receive(:puts).with("You have 2 hour(s) left to keep having fun!")
     require_relative '../lib/deluxe_curfew_checker.rb'
     platinum_curfew_checker(9, 11)
   end
